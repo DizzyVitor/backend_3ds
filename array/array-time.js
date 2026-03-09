84b2d6c2f86998548a5313b9923f514c70dfeb03
DizzyVitor <vitor.tizott@escola.pr.gov.br>
2026-03-09 11:34:33 +0000

    método arrays:inserção, deleção e impressão

---
+++ array/array-time.js
@@ -0,0 +1,15 @@
+const time = ["Julius", "chris",]
+// indices       0         1
+time[5] // undefined
+time[6] = "Victor" // atribuição dinamicos
+
+
+for (const jogador of time) {
+    if (jogador != undefined) {
+        
+    }
+console.log(jogador)    
+}
+
+console.log(time)
+console.log(time[1])
