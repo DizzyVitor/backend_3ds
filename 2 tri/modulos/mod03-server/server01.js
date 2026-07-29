d5fffd0dcdaf3924036ce676ae171a559912bb7c
DizzyVitor <vitor.tizott@escola.pr.gov.br>
2026-07-29 11:40:12 +0000

    Criar servidor http com tratamento localhost

--- 2 tri/modulos/mod03-server/server01.js
+++ 2 tri/modulos/mod03-server/server01.js
@@ -1,12 +1,22 @@
 const http = require('node:http')
 const porta = 8081
 
-const server = http.createServer((req, res)=>{   
-res.statusCode = '204'
-res.setHeader('Content-Type', 'text/html',/
-res.end('Página Inicial')
+const sever = http.createServer((req, res)=>{
+    const novaUrl = new URL(req.url, `https://${req.headers.host}`) //http://localhost
+    const caminhoUrl = novaUrl.pathname
+    if(caminhoUrl === '/'){
+
+        res.statusCode = '201'
+        res.setHeader('Content-Type', 'text/jtml; charset=utf-8')
+        res.end('<h3>HTML Rocks!</h3>')
+    } else {
+        res.statusCode = '401'
+        res.setHeader('Content-Type', 'text/html; charset=utf-8')
+        res.end('<h3>401 Não autorizado</h3>')
+    }
+
 })
 server.listen(porta, ()=>{
-    console.log(`Servidor rodando na porta ${porta}`)
-    http://localhost:${porta}`)
-})  
+    console.log(`Servidor rodando na porta
+        http://local""host:${porta}`)
+})
